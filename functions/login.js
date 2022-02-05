8bb0de6ad18761be9bb167a208796f2d5ebc78b8
Necromantian <wjurczyszyn@gmail.com>
2022-02-05 17:37:25 +0100

    functions test

--- functions/login.js
+++ functions/login.js
@@ -6,11 +6,17 @@
 const qs = require('querystring');
 
 exports.handler = async (event) => {
-    const { login, password } = qs.parse(event.body);
+    if (!event.body) {
+        return {
+            statusCode: 400,
+        };
+    }
+    const { login, password } = JSON.parse(event.body);
 
     const isLogged = login === 'a' && password === 'a'
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ isLogged, login, password }),
+        body: JSON.stringify({ isLogged }),
     };
 };
