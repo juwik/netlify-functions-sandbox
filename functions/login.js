bbe3d9ac63ce07e8298d8050ac48ab5f77bd0a1e
Wiktor Jurczyszyn <wjurczyszyn@gmail.com>
2022-02-13 20:17:57 +0100

    cors fix

--- functions/login.js
+++ functions/login.js
@@ -3,20 +3,27 @@
 //     body: 'log in please',
 // });
 
-const qs = require('querystring');
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
+
+const qs = require("querystring");
 
 exports.handler = async (event) => {
-    if (!event.body) {
-        return {
-            statusCode: 400,
-        };
-    }
-    const { login, password } = JSON.parse(event.body);
-
-    const isLogged = login === 'a' && password === 'a'
-
+  if (!event.body) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({ isLogged }),
+      statusCode: 400,
     };
+  }
+  const { login, password } = JSON.parse(event.body);
+
+  const isLogged = login === "a" && password === "a";
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({ isLogged }),
+  };
 };
